5c235c8bb95fc9fac63da98260529c15ce66a711
dmitry4k <dmitry.k48@yandex.ru>
2023-05-22 22:36:01 +0300

    add proxysql

--- database/database.cpp
+++ database/database.cpp
@@ -19,6 +19,29 @@ namespace database{
         _pool = std::make_unique<Poco::Data::SessionPool>(Poco::Data::MySQL::Connector::KEY, _connection_string);
     }
 
+    size_t Database::get_max_shard(){
+        return 2;
+    }
+
+    std::vector<std::string> Database::get_all_hints(){
+        std::vector<std::string> result;
+        for(size_t i=0;i<=get_max_shard();++i){
+            std::string shard_name = "-- sharding:";
+            shard_name += std::to_string(i);
+            result.push_back(shard_name);
+        }
+        return result;
+    }
+
+    std::string Database::sharding_hint(std::string& str){
+
+        size_t shard_number = std::hash<std::string>{}(str)%get_max_shard();
+
+        std::string result = "-- sharding:";
+        result += std::to_string(shard_number);
+        return result;
+    }
+
     Database& Database::get(){
         static Database _instance;
         return _instance;
